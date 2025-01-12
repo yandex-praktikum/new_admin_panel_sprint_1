335111cd3c16721697aa1177a79f6a7c8f8c1d5f
artem262626 <artem.makarskiy@yandex.ru>
2025-01-13 02:02:24 +0300

    Исправление models.py

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -9,7 +9,6 @@ class TimeStampedMixin(models.Model):
     modified = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        # Этот параметр указывает Django, что этот класс не является представлением таблицы
         abstract = True
 
 
@@ -20,23 +19,15 @@ class UUIDMixin(models.Model):
         abstract = True
 
 class Genre(UUIDMixin, TimeStampedMixin):
-    # Типичная модель в Django использует число в качестве id. В таких ситуациях поле не описывается в модели.
-    # Вам же придётся явно объявить primary key.
-    # Первым аргументом обычно идёт человекочитаемое название поля
     name = models.CharField(_('name'), max_length=255) # перевод _('') name
-    # blank=True делает поле необязательным для заполнения.
     description = models.TextField(_('description'), blank=True, null=True)
-    # auto_now_add автоматически выставит дату создания записи 
 
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"genre"
-        # Следующие два поля отвечают за название модели в интерфейсе
-        verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры'
+        verbose_name = _('Genre')
+        verbose_name_plural = _('Genres')
 
 class Filmwork(UUIDMixin, TimeStampedMixin):
-    # Выбор жанра TextChoices movie, tv show, soap_opera, cartoon
     class Type(models.TextChoices):
         movie = ('movie', _('Фильм')) # Перевод Фильм
         tv_show = ('tv_show', _('Шоу')) # Перевод Тв Шоу
@@ -48,50 +39,66 @@ class Filmwork(UUIDMixin, TimeStampedMixin):
         default=Type.movie
     ) 
     title = models.CharField(_('title'), max_length=255) 
-    # Описание Фильма
     description = models.TextField(_('description'), blank=True, null=True)
-    # Поле для хранения даты создания фильма
     creation_date = models.DateField(_('Дата создания'),  blank=True, null=True)
-    # Выбор рейтинга от 0 до 100, по умолчанию равен 0
     rating = models.FloatField(_('rating'), blank=True, null=True, default=0,
              validators=[MinValueValidator(0),
              MaxValueValidator(100)])
 
     genres = models.ManyToManyField(Genre, through='GenreFilmwork')
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"film_work"
-        # Следующие два поля отвечают за название модели в интерфейсе
-        verbose_name = 'Кинопроизведение'
-        verbose_name_plural = 'Кинопроизведения'
+        verbose_name = _('Film')
+        verbose_name_plural = _('Films')
+        indexes = [
+            models.Index(fields=["creation_date"])
+        ]
 
 
 class GenreFilmwork(UUIDMixin):
-    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
-    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    film_work = models.ForeignKey(Filmwork, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-
     class Meta:
         db_table = "content\".\"genre_film_work"
+        indexes = [
+            models.Index(fields=["film_work_id", "genre_id"])
+        ]
+
 
 
 class Person(UUIDMixin, TimeStampedMixin):
-    full_name =  models.TextField(_('full_name'), blank=True) # перевод full_name
+    full_name =  models.TextField(_('full_name'), blank=True)
     persons  = models.ManyToManyField('Person', through='PersonFilmwork')
     class Meta:
         db_table = "content\".\"person"
-    # Следующие два поля отвечают за название модели в интерфейсе
-        verbose_name = 'Участника'
-        verbose_name_plural = 'Участники Кинопроизведения'
+        verbose_name = _('Person')
+        verbose_name_plural = _('Persons')
 
 
 
 class PersonFilmwork(UUIDMixin):
-    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
-    person = models.ForeignKey('Person', on_delete=models.CASCADE)
-    role = models.CharField(_('role'), max_length=255)
+    FILM_ROLE_CHOICES = [
+        ('director', _('Director')),
+        ('actor', _('Actor')),
+        ('producer', _('Producer')),
+        ('writer', _('Writer')),
+        ('actress', _('Actress')),
+        ('cinematographer', _('Cinematographer')),
+        ('editor', _('Editor')),
+        ('production designer', _('Production Designer')),
+        ('costumer', _('Costumer')),
+        ('sound designer', _('Sound Designer')),
+        ('gaffer', _('Gaffer')),
+    ]
+    film_work = models.ForeignKey(Filmwork, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    role = models.CharField(_('role'), max_length=255, choices=FILM_ROLE_CHOICES)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "content\".\"person_film_work"
+        indexes = [
+            models.Index(fields=["film_work_id", "person_id", "role"])
+        ]
 
